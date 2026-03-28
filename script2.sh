0fb9fbe4a34a78cb9ee82cee84b2682b2ec7f71d
Mohinee <mohinee824@gmail.com>
2026-03-28 21:54:24 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-if /Applications/VLC.app/Contents/MacOS/VLC --version &> /dev/null
-then
-    echo "VLC is installed"
+# Script 2: FOSS Package Inspector
+
+PACKAGE="vlc"
+
+# Check if package is installed
+if dpkg -l | grep -q $PACKAGE; then
+    echo "$PACKAGE is installed."
+    dpkg -l | grep $PACKAGE | awk '{print "Version: "$3}'
 else
-    echo "VLC not installed"
+    echo "$PACKAGE is NOT installed."
 fi
+
+# Case statement for philosophy notes
+case $PACKAGE in
+    vlc) echo "VLC: plays anything, built by students in Paris" ;;
+    httpd) echo "Apache: the web server that built the open internet" ;;
+    mysql) echo "MySQL: open source at the heart of millions of apps" ;;
+    firefox) echo "Firefox: nonprofit fighting for an open web" ;;
+    *) echo "Unknown package" ;;
+esac
